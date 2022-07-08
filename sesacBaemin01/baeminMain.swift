25adaf627e814299f3b806619bc970c369790b7e
peisns <hansol1501@gmail.com>
2022-07-08 09:40:15 +0900

    Initial Commit

---
+++ sesacBaemin01/baeminMain.swift
@@ -0,0 +1,31 @@
+//
+//  baeminMain.swift
+//  sesacBaemin01
+//
+//  Created by Brother Model on 2022/07/05.
+//
+
+import UIKit
+
+class baeminMain: UIViewController {
+
+    @IBOutlet weak var category01: UIButton!
+    @IBOutlet weak var category02: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        category01.setImage(self.resizeImage(image: UIImage(named: "shop.png")!, width: 70, height: 70), for: UIControl.State.normal)
+        
+    }
+    
+    func resizeImage(image: UIImage, width: CGFloat, height: CGFloat) -> UIImage {
+        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+        image.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
+
+}
